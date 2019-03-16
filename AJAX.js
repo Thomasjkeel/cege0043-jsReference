380351a153edfacb432f241d48d67646fbb3b5cb
Thomasjkeel <thomasjames.keel@googlemail.com>
2019-03-16 13:34:26 -0400

    update xml req

--- AJAX.js
+++ AJAX.js
@@ -2,12 +2,22 @@ var xhrNode;
 
 function callDivNodeJSChange() {
     xhrNode = new XMLHttpRequest();
-    // var filename = document.getElementById("filename").value;
+    var filename = document.getElementById("filename").value;
+    // console.log( url + '/' + filename);
+    // xhrNode.open("GET", url + '/' + filename , true); // url + '/' + filename
+    // xhrNode.onreadystatechange = processDivNodeJSChange;
+    // xhrNode.send();
     var url = "http://developer.cege.ucl.ac.uk:" + httpPortNumber;
     console.log(url + '/' + filename);
-    xhrNode.open("GET", httpPortNumber, true); // url + '/' + filename
+    xhrNode.open("GET", url +'/' + filename, true);
     xhrNode.onreadystatechange = processDivNodeJSChange;
-    xhrNode.send();
+    try {
+        xhrNode.setRequestHeader("Content-Type", "application/x-www-form-urlencoded ");
+        console.log("here")
+        }
+        catch (e) {}
+        // this only works in internet explorer
+        xhrNode.send();
 }
 
 function processDivNodeJSChange() {
@@ -15,7 +25,9 @@ function processDivNodeJSChange() {
         document.getElementById('ajaxtest').innerHTML = "Loading...";
     } else if (xhrNode.readyState === 4) {
         if (xhrNode.status == 200 && xhrNode.status < 300) {
+
             document.getElementById('ajaxtest').innerHTML = xhrNode.responseText;
+        } else {
         }
     }
 }
