09a7e8723742551cb5125fa5d4fbb05ec5d976cd
Thomasjkeel <thomasjames.keel@googlemail.com>
2019-03-17 15:45:26 -0400

    change order for startup

--- startup.js
+++ startup.js
@@ -13,7 +13,7 @@ function trackAndCircle() {
 
 function startup() {
   document.addEventListener('DOMContentLoaded', function() {
-    trackAndCircle ();
     getPort();
+    trackAndCircle();
   }, false);
 }
